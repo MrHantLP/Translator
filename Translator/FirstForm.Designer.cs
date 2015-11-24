17c172b8281a0ca4d9a7c54a4806359492faaa94
MrHantLP <mrhantbk@gmail.com>
2015-11-25 07:50:24 +1000

    Добавлена Инициализация граматики

--- Translator/FirstForm.Designer.cs
+++ Translator/FirstForm.Designer.cs
@@ -32,11 +32,12 @@
             this.ChosePath = new System.Windows.Forms.Button();
             this.RUN = new System.Windows.Forms.Button();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // FileLocation
             // 
-            this.FileLocation.Location = new System.Drawing.Point(12, 12);
+            this.FileLocation.Location = new System.Drawing.Point(15, 30);
             this.FileLocation.Name = "FileLocation";
             this.FileLocation.ReadOnly = true;
             this.FileLocation.Size = new System.Drawing.Size(348, 20);
@@ -45,7 +46,7 @@
             // 
             // ChosePath
             // 
-            this.ChosePath.Location = new System.Drawing.Point(366, 10);
+            this.ChosePath.Location = new System.Drawing.Point(369, 28);
             this.ChosePath.Name = "ChosePath";
             this.ChosePath.Size = new System.Drawing.Size(75, 23);
             this.ChosePath.TabIndex = 1;
@@ -55,11 +56,11 @@
             // 
             // RUN
             // 
-            this.RUN.Location = new System.Drawing.Point(325, 40);
+            this.RUN.Location = new System.Drawing.Point(268, 56);
             this.RUN.Name = "RUN";
-            this.RUN.Size = new System.Drawing.Size(116, 23);
+            this.RUN.Size = new System.Drawing.Size(176, 23);
             this.RUN.TabIndex = 2;
-            this.RUN.Text = "Провести анализ";
+            this.RUN.Text = "Провести трансляцию";
             this.RUN.UseVisualStyleBackColor = true;
             this.RUN.Click += new System.EventHandler(this.RUN_Click);
             // 
@@ -69,11 +70,21 @@
             this.openFileDialog.FileName = "*.pas";
             this.openFileDialog.Filter = "Код на языке Pascal (.pas)|*.pas";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Путь к файлу для трансляции";
+            // 
             // FirstForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 75);
+            this.ClientSize = new System.Drawing.Size(459, 91);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.RUN);
             this.Controls.Add(this.ChosePath);
             this.Controls.Add(this.FileLocation);
@@ -92,6 +103,7 @@
         private System.Windows.Forms.Button ChosePath;
         private System.Windows.Forms.Button RUN;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label1;
     }
 }
 
